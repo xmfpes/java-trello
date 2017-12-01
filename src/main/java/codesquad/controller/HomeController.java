1c24ec93368a971f55f2c2fd2088f6d4a5108fbb
KyuNam <xmfpes@naver.com>
2017-12-01 19:19:11 +0900

    Feat(Controller) - create default controller feature

---
+++ src/main/java/codesquad/controller/HomeController.java
@@ -0,0 +1,24 @@
+package codesquad.controller;
+
+import javax.servlet.http.HttpServletRequest;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class HomeController {
+	@GetMapping("")
+	public String index() {
+		return "index";
+	}
+	@GetMapping("/signUp")
+	public String signUpForm() {
+		return "signUp";
+	}
+	@GetMapping("/login")
+	public String loginForm(HttpServletRequest req) {
+		String referer = req.getHeader("Referer");
+		req.getSession().setAttribute("prevPage", referer);
+		return "login";
+	}
+}
